1b6db9bdaaea31c6e682775ab903850ec13b9472
luzh <luzh@eng.ucsd.edu>
2015-12-09 12:29:54 -0800

    Update wxPython test script.

--- python/2.7/wxpython/imagetest.py
+++ python/2.7/wxpython/imagetest.py
@@ -1,33 +1,52 @@
 #!/usr/local/bin/python2.7
 
-import sys
-import os
+# Code from http://wiki.wxpython.org/AnotherTutorial
 
-# Display using WXPython
-import matplotlib
-matplotlib.use('WXAgg')
+import wx
 
-import numpy as np
-import matplotlib.pyplot as plt
+class Colours(wx.Dialog):
+  def __init__(self, parent, id, title):
 
-from PIL import Image
+    wx.Dialog.__init__(self, parent, id, title, size=(300, 300))
+    vbox = wx.BoxSizer(wx.VERTICAL)
+    self.pnl1 = wx.Panel(self, -1)
+    self.pnl2 = wx.Panel(self, -1)
+    self.pnl3 = wx.Panel(self, -1)
+    self.pnl4 = wx.Panel(self, -1)
+    self.pnl5 = wx.Panel(self, -1)
+    self.pnl6 = wx.Panel(self, -1)
+    self.pnl7 = wx.Panel(self, -1)
+    self.pnl8 = wx.Panel(self, -1)
 
-# Unbuffer prints
-unbuffered = os.fdopen(sys.stdout.fileno(), 'w', 0)
-sys.stdout = unbuffered
+    gs = wx.GridSizer(4,2,3,3)
+    gs.AddMany([ (self.pnl1, 0 ,wx.EXPAND),
+                 (self.pnl2, 0, wx.EXPAND),
+                 (self.pnl3, 0, wx.EXPAND),
+                 (self.pnl4, 0, wx.EXPAND),
+                 (self.pnl5, 0, wx.EXPAND),
+                 (self.pnl6, 0, wx.EXPAND),
+                 (self.pnl7, 0, wx.EXPAND),
+                 (self.pnl8, 0, wx.EXPAND) ])
 
-# --- #
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+    vbox.Add(gs, 1, wx.EXPAND | wx.TOP, 5)
+    self.SetSizer(vbox)
+    self.SetColors()
+    self.Centre()
+    self.ShowModal()
+    self.Destroy()
 
-print "The container is working properly if a plot shows a sigmoid function."
+  def SetColors(self):
+    self.pnl1.SetBackgroundColour(wx.BLACK)
+    self.pnl2.SetBackgroundColour(wx.Colour(139,105,20))
+    self.pnl3.SetBackgroundColour(wx.RED)
+    self.pnl4.SetBackgroundColour('#0000FF')
+    self.pnl5.SetBackgroundColour('sea green')
+    self.pnl6.SetBackgroundColour('midnight blue')
+    self.pnl7.SetBackgroundColour(wx.LIGHT_GREY)
+    self.pnl8.SetBackgroundColour('plum')
 
-print "WXAgg is used as the rendering backend."
+print "The container is working properly if a window shows eight color blocks."
 
-x = np.arange(-8, 8, 0.1);
-plt.plot(x, sigmoid(x), label="sigmoid(x)")
-plt.grid(True)
-plt.xlabel("x")
-plt.ylabel("sigmoid")
-plt.show()
-plt.savefig("sigmoid.png")
+app = wx.App(0)
+Colours(None, -1, 'Colours')
+app.MainLoop()
